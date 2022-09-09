899d5f86baa9e40d95c988fd65ddc6f1d9612223
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-09-09 13:37:51 +0200

    hov det her er den rigtige

--- src/SuperGreeker.java
+++ src/SuperGreeker.java
@@ -1,8 +1,14 @@
 import com.sun.tools.javac.Main;
 
+import java.util.Scanner;
+
 public class SuperGreeker {
     public void start(){
         System.out.println("det virker");
+        System.out.println("hva hedder du?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("hej " + name + " hvor gammel er du?");
     }
 
     public static void main(String[] args){
